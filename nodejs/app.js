8dfb143044fe7346335a09ec337f060d7116bae4
nabilkannane <nabilkannane1@gmail.com>
2023-01-26 16:25:24 +0100

    bbbb

--- nodejs/app.js
+++ nodejs/app.js
@@ -6,14 +6,16 @@ const path = require('path')
 const app = express();
 
 // ============= middlewares ==================
-app.use(express.static(path.resolve(__dirname, '../reactjs/build')));
 
 app.use( cors() ); // controler l'acces a l'API
 
 app.use('/api', venteRoutes) //Routes de vente
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../reactjs/build', 'index.html'));
-});
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.resolve(__dirname, '../reactjs/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../reactjs/build', 'index.html'));
+  });
+}
 
 module.exports = app;
