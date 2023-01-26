b5e30b89e6be6b8f494bad80580b849e2a1c3795
nabilkannane <nabilkannane1@gmail.com>
2023-01-26 18:49:23 +0100

    ready for vercel

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use( cors() ); // controler l'acces a l'API
 app.use('/api', venteRoutes) //Routes de vente
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.resolve(__dirname, './build')));
+  app.use(express.static(path.resolve(__dirname, './reactjs/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './reactjs/build', 'index.html'));
   });
 }
 
